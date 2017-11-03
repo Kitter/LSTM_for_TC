56de45968b67d90d205f021c4b1852373cc97a38
Sookyung Kim <kim79@llnl.gov>
2017-11-03 06:00:49 -0700

    Add files via upload

--- main_2.py
+++ main_2.py
@@ -16,7 +16,7 @@ fout_pres=open("pred_s.txt","w")
 fout_gt=open("gt.txt","w")
 fout_pre.write("\nPrediction \n"); fout_gt.write("\nGround Truth \n");
 # Import Input data : Takes long time
-train_size=2500; test_size=400; val_size=10;
+train_size=2000; test_size=50; val_size=50;
 w=129;h=86; 
 #Before start please run "python read_data.py" to save input file#
 tr_input_image=np.load("/export/kim79/h2/finished_npy/7/tr_input_batch.npy")
@@ -37,7 +37,7 @@ va_output_lonlat=np.expand_dims(va_output_lonlat,axis=3)
 validation_step=10;
 learning_rate =0.0005
 training_steps =200000
-input_size =timesteps=24; 
+input_size =timesteps=4; 
 feature_size=w*h;
 channels=2;
 
@@ -84,53 +84,59 @@ loss_lonlat=tf.div(sqsum,Y_sum)
 print(tf.shape(Y_sum));
 print(tf.shape(loss_lonlat));
 
-loss_op=loss_lonlat
+mse=loss_op=loss_lonlat
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 train_op = optimizer.minimize(loss_op)
 
-#Evaluate Model
-mse=loss_lonlat;
-num_epoch=20000;
-epsilon=0.000001;
-
-#Initialize the variables
-init = tf.global_variables_initializer()
-# Start training
 with tf.Session() as sess:
-    # Run the initializer
+    # Initialize all variables
+    saver = tf.train.Saver()
+    val_best_loss=0.01;
+    val_best_step=0;
+    num_epoch=1000;
+    alpha=0.9
+    train_dir = 'best_model'
+    os.system('mkdir -p {}'.format(train_dir))
+    init = tf.global_variables_initializer()
     sess.run(init)
-    delta=1;
-    step=0;
-    loss=1;
-    while(delta>epsilon and step<train_size*num_epoch):
-        step_i=step%int(train_size);
+    for step in range(0,num_epoch*train_size):
+        #Load Data
+        step_i=int(step%train_size);
+        j=int(step%val_size);
         batch_x, batch_y_state, batch_y_lonlat = tr_input_image[step_i], tr_output_state[step_i], tr_output_lonlat[step_i];       
-        fout_log.write(str(step)+"\n");
         # Run optimization op (backprop)
         sess.run(train_op, feed_dict={X: batch_x, Y_state: batch_y_state, Y_lonlat: batch_y_lonlat})
-        ll=step%validation_step
-        if ll == 0 :
-            print(step,delta,loss);
-            j=int(step/validation_step)%val_size
-            # Calculate batch loss in validation set
-            loss= sess.run(mse, feed_dict={X: va_input_image[j], Y_state:va_output_state[j], Y_lonlat: va_output_lonlat[j]})
-            if j > 0 : delta=abs(validation_loss-float(loss));
-            fout_log.write("Step " + str(step) + ", Minibatch Loss= " + \
-                  "{:.4f}".format(loss) + \
-                  "{:.4f}".format(delta) + "\n")
-            validation_loss=float(loss);
-        step=step+1;
+        # Calculate batch loss in validation set
+        loss= sess.run(mse, feed_dict={X: va_input_image[j], Y_state:va_output_state[j], Y_lonlat: va_output_lonlat[j]})
+        #Calculated running average of val_loss
+        if step == 10:  # loss start from very large val at step=0, so start from 10th step
+            val_loss = loss
+        elif step >10:
+            val_loss = alpha * val_loss + (1-alpha) * loss
+            #write up
+            fout_log.write("Step " + str(step) + ", Validation Loss= " + \
+                      "{:.4f}".format(val_loss) + ", Loss= "+\
+                      "{:.4f}".format(loss) + "\n")
+            print("Step " + str(step) + ", Validation Loss= " + \
+                      "{:.4f}".format(val_loss) + ", Loss= "+\
+                      "{:.4f}".format(loss) + "\n")
+        if step > 10 and val_loss < val_best_loss:
+            val_best_loss = val_loss
+            saver.save(sess, os.path.join(train_dir, 'model-val_best'), global_step=step)
+            print('found new best validation loss:', val_loss)
+    ###################################################################
     #After finishing training Write Test result as output npy files
     print("DONE and writing test data")
+    saver.restore(sess, os.path.join(train_dir, 'model-val_best-{}'.format(val_best_step)))
     #Load test data set one
     pre_list=[]; gt_list=[];
-    for stepp in range(len(te_output_state)):
-        te_batch_x, te_batch_y_state, te_batch_y_lonlat = te_input_image[stepp], te_output_state[stepp], te_output_lonlat[stepp];
-        pre=sess.run(prediction_lonlat,feed_dict={X:te_batch_x, Y_state:te_batch_y_state, Y_lonlat: te_batch_y_lonlat});
-        gt=sess.run(Y_lonlat,feed_dict={X:te_batch_x, Y_state:te_batch_y_state, Y_lonlat: te_batch_y_lonlat});
-        pre_list.append(pre);
-        gt_list.append(gt);
-        print("writing "+str(step)+"th testing result in set")
+    stepp=val_best_step
+    te_batch_x, te_batch_y_state, te_batch_y_lonlat = te_input_image[stepp], te_output_state[stepp], te_output_lonlat[stepp];
+    pre=sess.run(prediction_lonlat,feed_dict={X:te_batch_x, Y_state:te_batch_y_state, Y_lonlat: te_batch_y_lonlat});
+    gt=sess.run(Y_lonlat,feed_dict={X:te_batch_x, Y_state:te_batch_y_state, Y_lonlat: te_batch_y_lonlat});
+    pre_list.append(pre);
+    gt_list.append(gt);
+    print("writing "+str(step)+"th testing result in set")
     pre_list=np.asarray(pre_list)
     gt_list=np.asarray(gt_list)
     np.save("prediction_"+str(step)+".npy",pre_list)
